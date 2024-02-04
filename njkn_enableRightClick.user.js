8e9c606223b23c74c6d8d68ebdd89f6e921c03d1
yakkoworks <93868999+yakkoworks@users.noreply.github.com>
2024-02-04 19:20:00 +0900

    Add files via upload

---
+++ njkn_enableRightClick.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         右クリック禁止を禁止
+// @namespace    http://tampermonkey.net/
+// @version      2023-12-23
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.nizikano-2d.jp/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// @run-at context-menu
+// ==/UserScript==
+
+(function() {
+alart("ぜんぶミラクル");
+})();
